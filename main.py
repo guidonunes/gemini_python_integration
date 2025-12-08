30174f52e430cc6eac14fd65bf4e1c18df29bf35
Guilherme Nunes <guilherme.augd@gmail.com>
2025-12-08 16:46:16 -0300

    add model config and update prompt

--- main.py
+++ main.py
@@ -12,11 +12,21 @@ genai.configure(api_key=GOOGLE_API_KEY)
 CHOSEN_MODEL = "gemini-2.5-flash"
 
 
-system_prompt ="list five famous video games from the 1990s in a numbered format."
+system_prompt ="You are a gaming specialist. When asked you should recommend the user a list of three games to play based on their feelings and personal taste. Recommend both recent and classics, but give preference to games released after 2016."
+
+
+model_config = {
+    "temperature": 2.0,
+    "max_output_tokens": 8192,
+    "top_p": 0.8,
+    "top_k": 64,
+    "response_mime_type": "text/plain"
+}
 
 llm = genai.GenerativeModel(
     model_name=CHOSEN_MODEL,
-    system_instruction=system_prompt
+    system_instruction=system_prompt,
+    generation_config=model_config
     )
 
 
